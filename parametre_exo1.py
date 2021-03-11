228221d61cb238811c52e237e4a93f198615d329
Harold AO <contact@haroldao.com>
2021-03-11 14:00:21 +0000

    Add HTML Page

--- parametre_exo1.py
+++ parametre_exo1.py
@@ -2,8 +2,16 @@ import bottle
 
 @bottle.route('/')
 def index():
-  return """
-<h1> Hi </h1>
-<p> First Page. </p>
-"""
+  nom = bottle.request.query.nom
+  prenom = bottle.request.query.prenom
+#   return """
+# <h1> Hi </h1>
+# <p> First Page. </p>
+# """
+  # return bottle.template(
+  #   'Hi {{nom}} {{prenom}}'
+  #   , nom=nom, prenom=prenom)
+
+  return bottle.template("exo1.html", nom=nom, prenom=prenom)
+
 bottle.run(host='0.0.0.0', port=8080, debug=True, reloader=True)
