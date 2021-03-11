fcdda0e1aeb84d4a498b40dbbf4e0a5023cec510
Harold AO <contact@haroldao.com>
2021-03-11 13:28:27 +0000

    Add page 2 + Go to page 2 button

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import bottle
-@bottle.route('/')
 
+
+@bottle.route('/')
 def index():
   return """
-<h1> Coucou </h1>
-<p> Première tentative Bottle. </p>
+<h1> Hello World </h1>
+<p> First Try with Bottle. </p>
+
+<button>
+<a href="/page2">Go to Page 2</a>
+</button>
 """
-bottle.run(host='localhost', port=8080, debug=True, reloader=True)
+
+
+@bottle.route('/page2')
+def page2():
+  return """
+<h1> Page 2. </h1>
+<p> This is a second page </p>"""
+
+bottle.run(host='0.0.0.0', port=8080, debug=True, reloader=True)
